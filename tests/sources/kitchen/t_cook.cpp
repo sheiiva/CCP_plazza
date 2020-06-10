88941725378b794782f2c50528cc735218c065fd
Segfault <charles.verchere-garcia-de-mateos@epitech.eu>
2020-06-10 13:13:45 -0700

    [ADD] get status test

---
+++ tests/sources/kitchen/t_cook.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** t_cook.cpp
+** File description:
+** cook tests
+*/
+
+#include "cri_func.hpp"
+#include "Cook.hpp"
+
+Test(getStatus, get_Status, .init=redirect_all_std)
+{
+    Plazza::Cook cook;
+
+    cr_assert_eq(cook.getStatus, 0);
+}
